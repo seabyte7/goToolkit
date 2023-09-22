629d20f6c83835e1991052b0f458f616494b2746
seabyte7 <inkcpp@gmail.com>
2023-09-22 17:27:15 +0800

    add zap log to loglib

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module goToolkit
 
 go 1.19
+
+require (
+	go.uber.org/multierr v1.11.0 // indirect
+	go.uber.org/zap v1.26.0 // indirect
+)
--- logLib/loglib.go
+++ logLib/loglib.go
@@ -1,1 +1,59 @@
 package logLib
+
+import (
+	"fmt"
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
+	"os"
+)
+
+var (
+	loggerInst *zap.Logger
+)
+
+func StartLog(filePath string, logLevel zapcore.Level) {
+	// create standard file encoder
+	consoleEncoder := zapcore.NewConsoleEncoder(zap.NewDevelopmentEncoderConfig())
+	consoleFile, err := os.OpenFile(filePath, os.O_CREATE|os.O_RDWR|os.O_APPEND, 0644)
+	if err != nil {
+		panic(fmt.Errorf("open log file:%v error:%v", filePath, err.Error()))
+	}
+	consoleOutput := zapcore.AddSync(consoleFile)
+
+	// create json encoder
+	jsonEncoder := zapcore.NewJSONEncoder(zap.NewDevelopmentEncoderConfig())
+	jsonFilePath := filePath + "_json"
+	jsonFile, err := os.OpenFile(jsonFilePath, os.O_CREATE|os.O_RDWR|os.O_APPEND, 0644)
+	if err != nil {
+		panic(fmt.Errorf("open json log file:%v error:%v", jsonFilePath, err.Error()))
+	}
+	jsonOutput := zapcore.AddSync(jsonFile)
+
+	// create core
+	core := zapcore.NewTee(
+		zapcore.NewCore(consoleEncoder, consoleOutput, logLevel),
+		zapcore.NewCore(jsonEncoder, jsonOutput, logLevel),
+	)
+
+	// create zap log
+	logger := zap.New(core)
+	if err != nil {
+		panic(fmt.Errorf("new zaplog  failed:%v", err.Error()))
+	}
+
+	loggerInst = logger
+}
+
+func StopLog() {
+	if loggerInst != nil {
+		loggerInst.Sync()
+	}
+}
+
+func Zap() *zap.Logger {
+	return loggerInst
+}
+
+func Sugar() *zap.SugaredLogger {
+	return loggerInst.Sugar()
+}
