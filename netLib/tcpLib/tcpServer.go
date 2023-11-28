9121d3fd1d1b47d1d1818b98f9f1f90b944be5f4
seabyte7 <inkcpp@gmail.com>
2023-11-28 13:24:59 +0800

    调整tcpserver代码

--- netLib/tcpLib/tcpServer.go
+++ netLib/tcpLib/tcpServer.go
@@ -1,5 +1,48 @@
 package tcpLib
 
+import (
+	"goToolkit/logLib"
+	"net"
+	"sync"
+)
+
 type TcpServer struct {
-	SessionID uint64
+	Addr string
+
+	conn net.Conn
+
+	CloseOnce sync.Once
+	exitChan  chan bool
+}
+
+func NewTcpServer(addr string) *TcpServer {
+	return &TcpServer{
+		Addr: addr,
+		//CloseOnce:
+		exitChan: make(chan bool, 1),
+	}
+}
+
+func (this *TcpServer) Start() {
+	net.Listen("tcp", this.Addr)
+}
+
+func (this *TcpServer) Stop() {
+	this.CloseOnce.Do(func() {
+		close(this.exitChan)
+	})
+}
+
+func (this *TcpServer) acceptThread() {
+	logLib.Zap().Info("TcpServer acceptThread start.")
+
+	willExit := false
+	for {
+
+		select {
+		case <-this.exitChan:
+			willExit = true
+
+		}
+	}
 }
--- netLib/tcpLib/tcpSession.go
+++ netLib/tcpLib/tcpSession.go
@@ -1,7 +1,9 @@
 package tcpLib
 
 import (
+	"goToolkit/logLib"
 	"goToolkit/netLib/netType"
+	"io"
 	"net"
 	"sync"
 )
@@ -28,4 +30,67 @@ func newSession(conn net.Conn) *Session {
 		recvDataBuf:    make([]byte, 0),
 		exitChan:       make(chan struct{}, 1),
 	}
+}
+
+func (this *Session) recvThread() {
+	for {
+		recvBuf := make([]byte, 0, 1024)
+		count, err := this.conn.Read(recvBuf)
+		if err != nil {
+			if err == io.EOF {
+				logLib.Sugar().Infof("session:%d read eof, socket closed.", this.ID)
+				break
+			}
+			logLib.Sugar().Infof("session:%d read error:%v", this.ID, err)
+			break
+		}
+
+		this.recvDataBuf = append(this.recvDataBuf, recvBuf[:count]...)
+
+		msgList := this.parseMessage()
+		for _, msg := range msgList {
+			this.RecvMsgChannel <- msg
+		}
+	}
+}
+
+// parse message packet
+func (this *Session) parseMessage() []*netType.Message {
+	msgList := make([]*netType.Message, 0, 4)
+	maxTryCount := 4
+
+	for i := 0; i < maxTryCount; i++ {
+		msgLen := netType.BytesToUint32(this.recvDataBuf[:netType.MsgHeaderLen])
+		bufLen := len(this.recvDataBuf)
+		if msgLen < uint32(bufLen) {
+			return msgList
+		}
+
+		msg := netType.NewMessage(msgLen, this.recvDataBuf[:msgLen])
+		msgList = append(msgList, msg)
+
+		this.recvDataBuf = this.recvDataBuf[msgLen:]
+	}
+
+	return msgList
+}
+
+func (this *Session) sendThread() {
+	for {
+		msg := <-this.SendMsgChannel
+		count, err := this.conn.Write(msg.ToMsgBytes())
+		if err != nil {
+			logLib.Sugar().Errorf("session:%d write error:%v", this.ID, err)
+			break
+		}
+		logLib.Sugar().Debugf("session:%d write msg:%v, length:%d", this.ID, msg, count)
+	}
+}
+
+// send msg to server
+func (this *Session) SendMsg(data []byte) {
+	msgLen := netType.MsgHeaderLen + uint32(len(data))
+	msg := netType.NewMessage(msgLen, data)
+
+	this.SendMsgChannel <- msg
 }
